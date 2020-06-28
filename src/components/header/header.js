6277f5ad9fe14d634bfb107340d2a139722ee3a4
snigirevkonstantin1 <snigirevkonstantin1@gmail.com>
2020-06-28 23:48:24 +0300

    deploy files

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from './MarvelLogo.svg.png';
+import {useHistory} from 'react-router-dom'
+
+const Header = () => {
+    const history = useHistory();
+    return (
+        <header className='d-flex justify-content-center bg-dark'>
+            <img className='header__img' src={img} alt='logo' onClick={()=>history.push("/")}/>
+        </header>
+    )
+}
+
+export default Header
